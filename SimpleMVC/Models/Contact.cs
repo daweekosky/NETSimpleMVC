2a249d38ba56dd8ad1f5d10c4eaed103bd96fb02
daweekosky <dawid_czarnecki@o2.pl>
2024-02-12 09:34:50 +0100

    Created Contact class

---
+++ SimpleMVC/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace SimpleMVC.Models
+{
+    public class Contact
+    {
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Surname")]
+        public string Surname { get; set; }
+
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [DisplayName("City")]
+        public string City { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
