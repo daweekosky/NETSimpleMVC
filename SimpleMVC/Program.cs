2b4fb4134f429b879c600ff127c5b2988fe42eca
daweekosky <dawid_czarnecki@o2.pl>
2024-02-12 09:48:38 +0100

    PhoneBookService transformed into Singleton

--- SimpleMVC/Program.cs
+++ SimpleMVC/Program.cs
@@ -1,7 +1,11 @@
+using SimpleMVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<PhoneBookService>();
 
 var app = builder.Build();
 
